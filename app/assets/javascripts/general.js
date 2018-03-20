b3b59844455854f7dcbdf38de1d11e9e63be0b65
Geovany Gameros <geovanygameros@gmail.com>
2018-03-20 13:49:25 -0700

    Se implementa acceso con google

---
+++ app/assets/javascripts/general.js
@@ -0,0 +1,21 @@
+$(document).on('ready turbolinks:load', function() {
+    $("tr.tr-click").on('click', function () {
+        Turbolinks.visit($(this).attr("href"));
+    });
+
+
+    $('.button-collapse').sideNav({
+            menuWidth: 80, // Default is 300
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true, // Choose whether you can drag to open on touch screens,
+            onOpen: function(el) {}, // A function to be called when sideNav is opened
+            onClose: function(el) {}, // A function to be called when sideNav is closed
+        }
+    );
+
+
+    $('.modal').modal();
+
+
+});
