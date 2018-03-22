e80fffd80f5e533d41b5fef78fbc73dd952b80cb
Geovany Gameros <geovanygameros@gmail.com>
2018-03-22 16:01:55 -0700

    Se agrega sidebar como menú principal

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -5,7 +5,7 @@ $(document).on('ready turbolinks:load', function() {
 
 
     $('.button-collapse').sideNav({
-            menuWidth: 80, // Default is 300
+            menuWidth: 300, // Default is 300
             edge: 'left', // Choose the horizontal origin
             closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
             draggable: true, // Choose whether you can drag to open on touch screens,
@@ -17,5 +17,34 @@ $(document).on('ready turbolinks:load', function() {
 
     $('.modal').modal();
 
+    $('select').material_select();
+
+    $('.datepicker').pickadate({
+
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+
+        // The title label to use for the month nav buttons
+        labelMonthNext: 'Mes siguiente',
+        labelMonthPrev: 'Mes anterior',
+
+        // The title label to use for the dropdown selectors
+        labelMonthSelect: 'Selecciona un mes',
+        labelYearSelect: 'Selecciona un año',
+
+        // Months and weekdays
+        monthsFull: [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ],
+        monthsShort: [ 'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic' ],
+        weekdaysFull: [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado' ],
+        weekdaysShort: [ 'Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab' ],
+
+        // Materialize modified
+        weekdaysLetter: [ 'D', 'L', 'M', 'X', 'J', 'V', 'S' ],
+
+        // Today and clear
+        today: 'Hoy',
+        clear: 'Limpiar',
+        close: 'Cerrar'
+    });
 
 });
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,19 @@
 class Document < ApplicationRecord
   belongs_to :department
+  has_many :document_owners
+
+  MASTER_LIST = 1
+  PROCEDURE = 2
+  QUALITY_POLITICS = 3
+
+
+  DOCUMENT_TYPES ={
+      MASTER_LIST => 'Listado maestro',
+      PROCEDURE => 'Procedimiento',
+      QUALITY_POLITICS => 'Política de calidad'
+  }
+
+  def get_document_type
+    DOCUMENT_TYPES[self.document_type]
+  end
 end
